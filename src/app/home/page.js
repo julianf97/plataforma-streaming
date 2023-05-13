b1e3c2df7c7649b30a9e7c54f3006a20d027814e
Diego Gandara <diego.gandara@pedidosya.com>
2023-05-13 12:48:37 -0300

    feature: se configura styled-components en Next

---
+++ src/app/home/page.js
@@ -0,0 +1,16 @@
+'use client';
+import { Container, Parrafo } from './styled';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>SPA main page</h1>
+      <Parrafo>Hola Juli</Parrafo>
+      <Container>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </Container>
+    </main>
+  );
+}
---
+++ src/app/home/styled.js
@@ -0,0 +1,11 @@
+import Styled from 'styled-components';
+
+export const Parrafo = Styled.p`
+  color: red;
+  font-size: 26px;
+`;
+
+export const Container = Styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
