d6da47ff8748f1d05521e85422043dd3a7268091
Diego Gandara <diego.gandara@pedidosya.com>
2023-06-12 18:51:11 -0300

    feature: creo un provedor con Context Api - a futuro se va a reemplazar con Redux Toolkit- para centralizar el estado. En un primer momento, se observa el uso de useFetch -llamadas reales a la api- pero, de momento, para ahorrarlas, se comenta y se carga el provider con mocks

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,18 @@
-"use client"
+'use client';
 import React from 'react';
 import SearchBar from './components/SearchBar';
-import "./globals.css"
+import './globals.css';
 import MovieCard from './components/Cards/MovieCard/MovieCard';
+import { MoviesAndSeriesProvider, useMoviesAndSeries } from './provider';
 
 export default function App({ children }) {
   return (
-    <main>
-        <SearchBar/>
-        <MovieCard/>
-      {children}
-    </main>
+    <MoviesAndSeriesProvider>
+      <main>
+        <SearchBar />
+        <MovieCard />
+        {children}
+      </main>
+    </MoviesAndSeriesProvider>
   );
 }
