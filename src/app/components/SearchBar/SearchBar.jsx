9109af24f4f29283dfc70c1c65bdaf5334da80ac
Diego Gandara <diego.gandara@pedidosya.com>
2023-06-19 13:54:16 -0300

    chore: de esta manera, se renderiza la lupa pequeña, pero empieza color negro en vez de blanco

--- src/app/components/SearchBar/SearchBar.jsx
+++ src/app/components/SearchBar/SearchBar.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { SearchBarContainer, SearchBarInput } from './SearchBar.styled';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  SearchBarContainer,
+  Lupa,
+  SearchBarInput,
+  LupaContainer,
+} from './SearchBar.styled';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
-
 export default function SearchBar() {
-
-  
   return (
     <SearchBarContainer>
-      <FontAwesomeIcon icon={faMagnifyingGlass} className='lupa'></FontAwesomeIcon>
-      <SearchBarInput placeholder='Search for movies or TV series'></SearchBarInput>
+      <LupaContainer>
+        <Lupa icon={faMagnifyingGlass} />
+      </LupaContainer>
+      <SearchBarInput placeholder="Search for movies or TV series"></SearchBarInput>
     </SearchBarContainer>
   );
 }
--- src/app/components/SearchBar/SearchBar.styled.js
+++ src/app/components/SearchBar/SearchBar.styled.js
@@ -1,34 +1,43 @@
-import { styled } from 'styled-components'
-
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const SearchBarContainer = styled.div`
-    width: 100%;
-    height: 30px;
-    display: flex;
-    align-items: center;
-    .lupa{
-        color: #fff;
-        font-size: 1.2rem;
-        margin-left: 10px;
-        margin-bottom: 2px;
-    }
-`
+  width: 100%;
+  height: 30px;
+  display: flex;
+  align-items: center;
+`;
 
+export const LupaContainer = styled.div`
+  max-height: 1.2rem;
+  font-size: 1.2rem;
+  color: #fff;
+  fill: #fff;
+`;
+
+export const Lupa = styled(FontAwesomeIcon).attrs(({ icon }) => ({
+  icon,
+}))`
+  color: #fff;
+  font-size: 1.2rem;
+  margin-left: 10px;
+  margin-bottom: 2px;
+`;
 
 export const SearchBarInput = styled.input`
-    width: 200px;
-    height: 100%;
-    display: flex;
-    border-radius: 0px 20px 20px 0px;
+  width: 200px;
+  height: 100%;
+  display: flex;
+  border-radius: 0px 20px 20px 0px;
+  outline: 0;
+  border: 0;
+  color: #fff;
+  padding-left: 15px;
+  &:focus {
     outline: 0;
     border: 0;
-    color: #fff;
-    padding-left: 15px;
-    &:focus {
-        outline: 0;
-        border: 0;
-    }
-    &::placeholder { 
+  }
+  &::placeholder {
     color: #ffffff9d;
-    }
-`
+  }
+`;
