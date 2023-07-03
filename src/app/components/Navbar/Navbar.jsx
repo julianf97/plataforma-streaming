c8fa5ce058147b7b9aabced48a4fabe2db63ff46
Julian Finelli <julianfinelli97@gmail.com>
2023-07-03 11:30:00 -0300

    Maquetado Navbar Ipad

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IconContainer, NavContainer, NavbarInnerContainer, NavbarContainer, UserContainer, NavInnerContainer, BorderAllContainer, MoviesIconContainer, BorderAllInterno, BorderAllUp, BorderAllDown, TVcontainer, BookMarkContainer } from './Navbar.styled';
+import { GralContainer, IconContainer, NavContainer, NavbarInnerContainer, NavbarContainer, UserContainer, NavInnerContainer, BorderAllContainer, MoviesIconContainer, BorderAllInterno, BorderAllUp, BorderAllDown, TVcontainer, BookMarkContainer } from './Navbar.styled';
 import ClapperBoard from '../ClapperBoard/ClapperBoard';
 import Square from '../Square/Square';
 import Film from '../Film/Film';
@@ -13,40 +13,41 @@ export default function Navbar() {
 
   
   return (
-    
-    <NavbarContainer>
-      <NavbarInnerContainer>
-        <IconContainer>
-          <ClapperBoard/>
-        </IconContainer>
-        <NavContainer>
-          <NavInnerContainer>
-            <BorderAllContainer>
-              <BorderAllInterno>
-                <BorderAllUp>
-                  <Square/><Square/>
-                </BorderAllUp>
-                <BorderAllDown>
-                  <Square/><Square/>
-                </BorderAllDown>
-              </BorderAllInterno>
-            </BorderAllContainer>
-            <MoviesIconContainer>
-              <Film/>
-            </MoviesIconContainer>
-            <TVcontainer>
-              <TV/>
-            </TVcontainer>
-            <BookMarkContainer>
-              <BookMark/>
-            </BookMarkContainer>
-          </NavInnerContainer>
-        </NavContainer>
-        <UserContainer>
-          <IconUser/>
-        </UserContainer>
-      </NavbarInnerContainer>
-    </NavbarContainer>
+    <GralContainer>
+      <NavbarContainer>
+        <NavbarInnerContainer>
+          <IconContainer>
+            <ClapperBoard/>
+          </IconContainer>
+          <NavContainer>
+            <NavInnerContainer>
+              <BorderAllContainer>
+                <BorderAllInterno>
+                  <BorderAllUp>
+                    <Square/><Square/>
+                  </BorderAllUp>
+                  <BorderAllDown>
+                    <Square/><Square/>
+                  </BorderAllDown>
+                </BorderAllInterno>
+              </BorderAllContainer>
+              <MoviesIconContainer>
+                <Film/>
+              </MoviesIconContainer>
+              <TVcontainer>
+                <TV/>
+              </TVcontainer>
+              <BookMarkContainer>
+                <BookMark/>
+              </BookMarkContainer>
+            </NavInnerContainer>
+          </NavContainer>
+          <UserContainer>
+            <IconUser/>
+          </UserContainer>
+        </NavbarInnerContainer>
+      </NavbarContainer>
+    </GralContainer >
   );
 }
 
--- src/app/components/Navbar/Navbar.styled.js
+++ src/app/components/Navbar/Navbar.styled.js
@@ -1,7 +1,16 @@
 import { styled } from 'styled-components'
 
+export const GralContainer = styled.div`
+    // Ipad
+    @media only screen and (min-width:601px) and (max-width:1025px){ 
+        display: flex;
+        justify-content: center;
+        width: 100%;
+    }
+`
 
 export const NavbarContainer = styled.div`
+    // Mobile
     @media (max-width: 600px) {
         flex-direction: column;
         width: 100%;
@@ -11,8 +20,21 @@ export const NavbarContainer = styled.div`
         background-color: #161d2f;
         justify-content: center;
     }
+    // Ipad
+    @media only screen and (min-width:601px) and (max-width:1025px){ 
+        flex-direction: column;
+        width: 95%;
+        height: 50px;
+        display: flex;
+        align-items: center;
+        background-color: #161d2f;
+        justify-content: center;
+        margin-top: 15px;
+        border-radius: 8px;
+    }
 `
 export const NavbarInnerContainer = styled.div`
+    // Mobile
     @media (max-width: 600px) {
         width: 90%;
         height: 80%;
@@ -20,8 +42,19 @@ export const NavbarInnerContainer = styled.div`
         align-items: center;
         background-color: #161d2f;
     }
+    // Ipad
+    @media only screen and (min-width:601px) and (max-width:1025px){ 
+        width: 90%;
+        height: 80%;
+        display: flex;
+        align-items: center;
+        background-color: #161d2f;
+
+
+    }
 `
 export const IconContainer = styled.div`
+    // Mobile
     @media (max-width: 600px) { 
         width: 50px;
         height: 100%;
@@ -30,8 +63,18 @@ export const IconContainer = styled.div`
         justify-content: center;
         align-items: center;
     } 
+    // Ipad
+    @media only screen and (min-width:601px) and (max-width:1025px){ 
+        width: 50px;
+        height: 100%;
+        display: flex;
+        background-color: #161d2f;
+        justify-content: center;
+        align-items: center;
+    }
 `
 export const NavContainer = styled.div`
+    // Mobile
     @media (max-width: 600px) { 
         width: 350px;
         height: 100%;
@@ -39,9 +82,20 @@ export const NavContainer = styled.div`
         background-color: #161d2f;
         justify-content: center;
         align-items: center;
+    }
+    // Ipad
+    @media only screen and (min-width:601px) and (max-width:1025px){ 
+        width: 700px;
+        height: 100%;
+        display: flex;
+        background-color: #161d2f;
+        justify-content: center;
+        align-items: center;
+
     } 
 `
 export const NavInnerContainer = styled.div`
+    // Mobile
     @media (max-width: 600px) { 
         width: 50%;
         height: 100%;
@@ -50,9 +104,20 @@ export const NavInnerContainer = styled.div`
         background-color: #161d2f;
         justify-content: center;
     } 
+    // Ipad
+    @media only screen and (min-width:601px) and (max-width:1025px){ 
+        width: 50%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        background-color: #161d2f;
+        justify-content: center;
+
+    }
 `
 
 export const UserContainer = styled.div`
+    // Mobile
     @media (max-width: 600px) { 
         width: 50px;
         height: 100%;
@@ -61,9 +126,20 @@ export const UserContainer = styled.div`
         justify-content: center;
         align-items: center;
     }
+    // Ipad
+    @media only screen and (min-width:601px) and (max-width:1025px){ 
+        width: 50px;
+        height: 100%;
+        display: flex;
+        background-color: #161d2f;
+        justify-content: center;
+        align-items: center;
+
+    }
 `
 
 export const BorderAllContainer = styled.div `
+    // Mobile
     @media (max-width: 600px) { 
         width: 25%;
         height: 80%;
@@ -73,8 +149,20 @@ export const BorderAllContainer = styled.div `
         align-items: center;
         margin-left: -6px;
     }
+    // Ipad
+    @media only screen and (min-width:601px) and (max-width:1025px){ 
+        width: 12%;
+        height: 80%;
+        display: flex;
+        background-color: #161d2f;
+        margin-left: -6px;
+        justify-content: center;
+        align-items: center;
+
+    }
 `
 export const BorderAllInterno = styled.div `
+    // Mobile
     @media (max-width: 600px) { 
         width: 70%;
         height: 70%;
@@ -83,9 +171,20 @@ export const BorderAllInterno = styled.div `
         flex-wrap: wrap;
         margin-top: 6px;
     }
+    // Ipad
+    @media only screen and (min-width:601px) and (max-width:1025px){ 
+        width: 70%;
+        height: 70%;
+        display: flex;
+        background-color: #161d2f;
+        flex-wrap: wrap;
+        margin-top: 6px;
+
+    }
 `
 
 export const BorderAllUp = styled.div `
+    // Mobile
     @media (max-width: 600px) { 
         width: 110%;
         height: 55%;
@@ -93,8 +192,18 @@ export const BorderAllUp = styled.div `
         background-color: #161d2f;
         margin-top: 0px;
     }
+    // Ipad
+    @media only screen and (min-width:601px) and (max-width:1025px){ 
+        width: 110%;
+        height: 55%;
+        display: flex;
+        background-color: #161d2f;
+        margin-top: 0px;
+
+    }
 `
 export const BorderAllDown = styled.div `
+    // Mobile
     @media (max-width: 600px) { 
         width: 100%;
         height: 55%;
@@ -102,8 +211,19 @@ export const BorderAllDown = styled.div `
         background-color: #161d2f;
         margin-top: -3px;
     }
+    // Ipad
+    @media only screen and (min-width:601px) and (max-width:1025px){ 
+        width: 100%;
+        height: 55%;
+        display: flex;
+        background-color: #161d2f;
+        margin-top: -3px;
+
+
+    }
 `
 export const MoviesIconContainer = styled.div `
+    // Mobile
     @media (max-width: 600px) { 
         width: 15%;
         height: 50%;
@@ -113,32 +233,69 @@ export const MoviesIconContainer = styled.div `
         align-items: center;
         margin-top: 1px;
         margin-left: 1px;
+    }
+    // Ipad
+    @media only screen and (min-width:601px) and (max-width:1025px){ 
+        width: 15%;
+        height: 50%;
+        display: flex;
+        background-color: #161d2f;
+        justify-content: center;
+        align-items: center;
+        margin-top: 1px;
+        margin-left: 1px;
+
     } 
 `
 export const TVcontainer = styled.div `
-@media (max-width: 600px) { 
-    width: 20px;
-    height: 50%;
-    display: flex;
-    background-color: #161d2f;
-    justify-content: center;
-    align-items: center;
-    margin-top: 1px;
-    margin-left: 15px;
+    // Mobile
+    @media (max-width: 600px) { 
+        width: 20px;
+        height: 50%;
+        display: flex;
+        background-color: #161d2f;
+        justify-content: center;
+        align-items: center;
+        margin-top: 1px;
+        margin-left: 15px;
+    }
+    // Ipad
+    @media only screen and (min-width:601px) and (max-width:1025px){ 
+        width: 20px;
+        height: 50%;
+        display: flex;
+        background-color: #161d2f;
+        justify-content: center;
+        align-items: center;
+        margin-top: 1px;
+        margin-left: 15px;
 
-} 
+    } 
 `
 export const BookMarkContainer = styled.div `
-@media (max-width: 600px) { 
-    width: 20px;
-    height: 50%;
-    display: flex;
-    background-color: #161d2f;
-    justify-content: center;
-    align-items: center;
-    margin-top: 1px;
-    margin-left: 12px;
-} 
+    // Mobile
+    @media (max-width: 600px) { 
+        width: 20px;
+        height: 50%;
+        display: flex;
+        background-color: #161d2f;
+        justify-content: center;
+        align-items: center;
+        margin-top: 1px;
+        margin-left: 12px;
+    }
+    // Ipad
+    @media only screen and (min-width:601px) and (max-width:1025px){ 
+        width: 20px;
+        height: 50%;
+        display: flex;
+        background-color: #161d2f;
+        justify-content: center;
+        align-items: center;
+        margin-top: 1px;
+        margin-left: 25px;
+
+    }   
 `
 
 
